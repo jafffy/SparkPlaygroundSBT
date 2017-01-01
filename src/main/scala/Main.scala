23527baed4b4ce59070f7548738bfe9151d706b5
Jaewon Choi <jaewon.james.choi@gmail.com>
2017-01-01 12:46:51 +0900

    Mimic quickstart

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,5 +1,17 @@
+import org.apache.spark.SparkContext
+import org.apache.spark.SparkContext._
+import org.apache.spark.SparkConf
+
 object Main {
   def main(args: Array[String]): Unit = {
-    println("Hello, World!")
+    val logFile = "/Users/jaewon-choi/word_count_maruta.txt"
+    val conf = new SparkConf()
+      .setAppName("Simple Application")
+    val sc = new SparkContext(conf)
+    val logData = sc.textFile(logFile, 2).cache()
+    val numAs = logData.filter(line => line.contains("a")).count()
+    val numBs = logData.filter(line => line.contains("b")).count()
+    println(s"Lines with a: $numAs, Lines with b: $numBs")
+    sc.stop()
   }
 }

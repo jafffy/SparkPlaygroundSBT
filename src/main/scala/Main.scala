71a012068786581c148711f103ed3b4c61865705
Jaewon Choi <jaewon.james.choi@gmail.com>
2017-01-02 22:03:26 +0900

    Add local cluster

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -7,6 +7,7 @@ object Main {
     val logFile = "/Users/jaewon-choi/word_count_maruta.txt"
     val conf = new SparkConf()
       .setAppName("Simple Application")
+      .setMaster("local[2]")
     val sc = new SparkContext(conf)
     val logData = sc.textFile(logFile, 2).cache()
     val numAs = logData.filter(line => line.contains("a")).count()
